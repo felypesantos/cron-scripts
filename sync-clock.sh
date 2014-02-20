d2004450bedcb68e89fa2fbf4c44d5757ece350b
Felipe <felypesantos@gmail.com>
2014-02-20 19:11:56 -0300

    sync-clock.sh
    Sincroniza relogio

---
+++ sync-clock.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+NTPDATE=/usr/sbin/ntpdate
+SERVER="10.160.0.60"
+
+# if running from cron (no tty available), sleep a bit to space
+# out update requests to avoid slamming a server at a particular time
+if ! test -t 0; then
+  MYRAND=$RANDOM
+  MYRAND=${MYRAND:=$$}
+
+  if [ $MYRAND -gt 9 ]; then
+    sleep `echo $MYRAND | sed 's/.*\(..\)$/\1/' | sed 's/^0//'`
+  fi
+fi
+
+$NTPDATE -su $SERVER
+
+# update hardware clock on Linux (RedHat?) systems
+if [ -f /sbin/hwclock ]; then
+  /sbin/hwclock --systohc
+fi
+
